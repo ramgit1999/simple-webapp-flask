87df32d4e679b605df206b2dcb145efafb559fd4
ramgit1999 <rameshsiriki007@gmail.com>
2023-10-12 12:56:29 +0530

    added new line

--- app.py
+++ app.py
@@ -10,5 +10,8 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/hi')
+def hello2():
+    return 'you are in myway'
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
